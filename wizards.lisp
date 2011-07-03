cc94be4c72df20545c55274a8da356fe12a6b2ac
Bruno Buss <bruno.buss@gmail.com>
2011-07-03 16:29:12 -0300

    Criando a própria estrutura REPL do jogo Wizards. (Conteúdo do cap 6)

--- wizards.lisp
+++ wizards.lisp
@@ -46,7 +46,7 @@
 			`(you see a ,obj in the floor.)))
 	(apply #'append (mapcar #'describe-obj (objects-at loc objs obj-locs)))))
 
-;; Outras funções
+;; Ações do jogador:
 
 (defun look ()
 	(append (describe-location *location* *nodes*)
@@ -81,5 +81,46 @@
 (defun inventory ()
 	(cons 'items- (objects-at 'body *objects* *object-locations*)))
 
+;; Interface com o usuário:
 
+(defun game-repl ()
+	(let ((cmd (game-read)))
+		(unless (eq (car cmd) 'quit)
+			(game-print (game-eval cmd))
+			(game-repl))))
 
+(defun game-read ()
+	(let ((cmd (read-from-string
+		(concatenate 'string "(" (read-line) ")"))))
+	
+		(flet ((quote-it (x)
+				(list 'quote x)))
+			(cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
+
+(defparameter *allowed-commands* '(look walk pickup drop inventory))
+
+(defun game-eval (sexp)
+	(if (member (car sexp) *allowed-commands*)
+		(eval sexp)
+		'(i do not know that command.)))
+
+(defun tweak-text (lst caps lit)
+	(when lst
+	(let ((item (car lst))
+	      (rest (cdr lst)))
+        
+        (cond ((eq item #\space) (cons item (tweak-text rest caps lit)))
+              ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+	      ((eq item #\") (tweak-text rest caps (not lit)))
+	      (lit (cons item (tweak-text reast nil lit)))
+	      ((or caps lit) (cons (char-upcase item) (tweak-text rest nil lit)))
+	      (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
+
+(defun game-print (lst)
+	(princ (coerce (tweak-text (coerce (string-trim "() "
+							(prin1-to-string lst))
+					   'list)
+				   t
+				   nil)
+			'string))
+	(fresh-line))
